b2d1dbd778cae1fa7967b528fb984fe400c9c7d5
Chaemin07 <gkc05075@gmail.com>
2025-03-25 15:48:27 +0900

    chore: 누락된 create_tables.sql 파일 스테이징 및 추가
    - resources/db 경로 내 프로젝트 초기 설정을 위한 SQL  스크립트 추가
    - git add 경로 지정으로 스테이징 누락 해결

---
+++ src/main/resources/db/create_tables.sql
@@ -0,0 +1,23 @@
+create DATABASE todo_db;
+
+use todo_db;
+
+-- 사용자 테이블
+CREATE TABLE User
+(
+    userId   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(50)  NOT NULL,
+    password VARCHAR(30)  NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Task
+(
+    taskId      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    userId      BIGINT       NOT NULL,
+    title       VARCHAR(100) NOT NULL,
+    description TEXT,
+    createdAt   DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt   DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (userId) REFERENCES User (userId) ON DELETE CASCADE
+);
